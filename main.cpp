6bbc215449b92859c4df3d6cb01b9d0434d85546
DropStone99 <DropStone99@users.noreply.github.com>
2021-05-25 15:15:28 +0200

    hello

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int x;
+    std :: cout << "Hello, World!" << std :: endl;
+    std :: cin >> x;
+    std :: cout << x;
     return 0;
 }
